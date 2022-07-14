ee16ce57f472dd9674c2e7a87ad640ab7535fa77
raibigo <danieloavr@gmail.com>
2022-07-14 01:36:35 -0400

    zzz

--- cajero/cajero.js
+++ cajero/cajero.js
@@ -1,9 +1,21 @@
+var imagenes = [];
+imagenes ["5"] = "billete5.png";
+imagenes ["10"] = "billete10.png";
+imagenes ["20"] = "billete20.png";
+imagenes ["50"] = "billete50.png";
+imagenes ["100"] = "billete100.png";
+
+
+
 class billetes
 {
     constructor(v, c)
     {
+    this.imagen = new Image();
     this.valor = v
     this.cantidad = c
+    this.imagen.src = imagenes[this.valor];
+    this.saldo = this.valor * this.cantidad;
     } 
 }
 
@@ -41,22 +53,42 @@ function entregarDinero()
         {
             if(e.cantidad > 0)
             {
-            resultado.innerHTML += resultado.innerHTML + e.cantidad + " billetes de " + e.valor + "<br />"
+                for(var money = 0; money <= e.cantidad; money++)
+                {
+                    document.body.appendChild(e.imagen) + "<br />";
+                }
+
+            
             }
         }
 
     }
 }
 
+
+
 var caja = [];
 var entregado = [];
-caja.push( new billetes(100, 7) );
+caja.push( new billetes(100, 8) );
 caja.push( new billetes(50, 10) );
 caja.push( new billetes(20, 15) );
 caja.push( new billetes(10, 20) );
 caja.push( new billetes(5, 20) );
-caja.push( new billetes(1, 30) );
 
+
+function restarDinero()
+{
+    var resta;
+
+    for(var x of entregado)
+    {
+        resta = x.valor - x.cantidad
+        console.log("valores")
+    }
+}
+
+
+var total = document.getElementById("total")
 var resultado = document.getElementById("resultado")
 var dinero = 0;
 var div = 0;
@@ -64,6 +96,8 @@ var papeles = 0;
 
 var boton = document.getElementById("extraer");
 boton.addEventListener("click", entregarDinero);
+
+
 
 
 
